cf584c64ef9ee5f60bd1567c75399322c0bf6ae8
WebMasterIT! <wspc777@yandex.ru>
2020-07-30 12:00:29 +0300

    Second Homework

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import { radioPlayerInit } from './radioPlayer.js';
-
+import { videoPlayerInit } from './videoPlayer.js';
+import { musicPlayerInit } from './musicPlayer.js';
 
 const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
