ae5dd53f43b93d8d6f9401da29bd3077088b844f
WebMasterIT! <wspc777@yandex.ru>
2020-07-30 12:38:59 +0300

    create radio code

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,20 @@
 export const radioPlayerInit = () => {
-    console.log('Radio Init');
+    const radio = document.querySelector('.radio');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioHeader = document.querySelector('.radio-header');
+    const radioNagation = document.querySelector('.radio-navigation');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radio.stop.disabled = 'true';
+
+    radioNagation.addEventListener('change', (event) => {
+        const target = event.target;
+        audio.src = 'target.dataset.radioStation';
+        audio.play();
+
+    })
 }
